2ab810b201642493dd359077d3d5f334bc1ae102
Mehyad <43511859+myadegari@users.noreply.github.com>
2024-05-17 18:34:29 +0330

    fix: style and visibility

--- src/App.tsx
+++ src/App.tsx
@@ -43,8 +43,8 @@ function App() {
   useEffect(() => {
     if (startDateStorage) {
       if (relapse) {
-        setStreak(moment(relapse).diff(moment(startDateStorage), 'days'))
 
+        setStreak(useTracker.currentDate.diff(moment(relapse), 'days'))
       }
       else {
         setStreak(useTracker.currentDate.diff(moment(startDateStorage), 'days'))
@@ -61,15 +61,15 @@ function App() {
   const today = new DateObject(useTracker.currentDate.format('YYYY/MM/DD hh:mm:ss')).convert(persian, persian_fa)
 
   return (
-    <div className="min-w-[100dvw] min-h-[100dvh] dark:bg-black bg-white grid place-items-center dark:text-slate-50 font-body transition-colors duration-500 ">
+    <div className="min-w-[100dvw] min-h-[100dvh] dark:bg-woodSmoke-950 bg-woodSmoke-50 grid place-items-center dark:text-slate-50 font-body transition-colors duration-500 ">
       <div className="grid gap-5 place-items-center">
         <div className="flex items-center gap-2">
           <p>{MainContent.theme} :</p>
-          <div className="flex gap-2 bg-slate-300 dark:bg-slate-500 w-[40px] h-[10px] rounded-full relative">
+          <div className="flex gap-2 bg-slate-300 dark:bg-slate-500 w-[40px] h-[15px] rounded-full relative">
             <span
               onClick={() => setTheme(!theme)}
 
-              className={classNames("w-[20px] h-[10px]  flex items-center justify-center rounded-full shadow-md absolute transition-all right-0 duration-500 cursor-pointer", { 'bg-slate-50 ': theme, ' bg-slate-800 translate-x-[-100%]': !theme })}>
+              className={classNames("w-[20px] h-[15px]  flex items-center justify-center rounded-full shadow-md absolute transition-all right-0 duration-500 cursor-pointer", { 'bg-slate-50 ': theme, ' bg-slate-800 translate-x-[-100%]': !theme })}>
 
             </span>
           </div>
@@ -93,14 +93,15 @@ function App() {
 
             const dayDate = new DateObject({ locale: persian_fa, calendar: persian }).toFirstOfMonth().add(day - 1, 'day')
             if (startDateStorage && dayDate.toUnix() < moment(startDateStorage).unix()) {
-              color = 'dark:bg-woodSmoke-950 bg-slate-200'
+              color = 'dark:bg-woodSmoke-900 bg-slate-200'
             } else if (dayDate.format() == useTracker.currentDatePersian.format()) {
-              color = 'bg-gradient-to-t from-blue-600 from-10% via-blue-900 via-90% to-blue-500 border dark:border-blue-900'
+              color = 'bg-gradient-to-t dark:from-blue-600 from-sky-400 from-10% dark:via-blue-900 via-sky-700 via-90% dark:to-blue-500 to-sky-400 border border-sky-400 dark:border-blue-900'
             }
             else {
               if (dayDate.toUnix() < moment(useTracker.currentDate).unix() && startDateStorage && dayDate.toUnix() >= moment(startDateStorage).unix()) {
-                color = atomDays[day - 1] ? 'bg-gradient-to-t from-green-600 from-10% via-green-900 via-90% to-green-500 border dark:border-green-900'
-                  : 'bg-gradient-to-t from-shiraz-600 from-10% via-shiraz-900 via-90% to-shiraz-500 border dark:border-shiraz-900'
+                color = atomDays[day - 1] 
+                  ? 'bg-gradient-to-t dark:from-green-600 from-lime-400 from-10% dark:via-green-900 via-green-600 via-90% dark:to-green-500 to-green-400 border border-lime-400 dark:border-green-900'
+                  : 'bg-gradient-to-t dark:from-shiraz-600 from-shiraz-400 from-10% dark:via-shiraz-900 via-red-600 via-90% dark:to-shiraz-500 to-shiraz-400 border border-shiraz-400 dark:border-shiraz-900'
               }
             }
             return <M.div
@@ -108,7 +109,7 @@ function App() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: Math.ceil(day * 0.1) * 0.3 }}
               key={day}
-              className={`lg:w-[50px] md:w-[40px] sm:w-[35px] w-[25px] aspect-square flex items-center justify-center ${color}`} >
+              className={`lg:w-[50px] md:w-[40px] sm:w-[35px] w-[25px] aspect-square rounded-md ${color}`} >
             </M.div>
           }
 
@@ -118,55 +119,52 @@ function App() {
         <M.div className="flex justify-between gap-2 mt-5 text-sm text-white sm:text-base">
           <AnimatePresence>
             {
-              startDateStorage &&
-              <M.button initial={{ opacity: 0, scale: 0 }}
+              startDateStorage &&(
+       
+                <M.button initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
                 whileTap={{ scale: 0.9 }}
                 exit={{ opacity: 0, scale: 0 }}
-                className="px-5 py-2 rounded-md opacity-70 bg-gradient-to-t from-woodSmoke-600 from-10% via-woodSmoke-900 via-90% to-woodSmoke-500 border dark:border-woodSmoke-900"
+                className="rounded-md px-5 py-2 bg-gradient-to-t dark:from-shiraz-600 from-shiraz-400 from-10% dark:via-shiraz-900 via-red-600 via-90% dark:to-shiraz-500 to-shiraz-400 border border-shiraz-400 dark:border-shiraz-900 cursor-pointer "
                 onClick={() => {
-                  setStartDateStorage(undefined)
-                  setRelapse(undefined)
-                  setStreak(0)
-                  setAtomDays(new Array(LastOfMonth.day).fill(true))
-                }}>{MainContent.clear}</M.button>
+                  if (startDateStorage) {
+                    setAtomDays((prev) => {
+                      const newArray = prev.slice()
+                      const today = new DateObject({ date: new Date, locale: persian_fa, calendar: persian })
+                      newArray[today.day - 1] = false
+                      return newArray
+                    })
+                    setRelapse(useTracker.currentDate)
+                  }
+                }
+                }
+              >
+                {MainContent.relapse}
+              </M.button>
+              )
             }
           </AnimatePresence>
 
           <M.button initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             whileTap={{ scale: 0.9 }}
-            className="rounded-md px-5 py-2 cursor-pointer opacity-70 bg-gradient-to-t from-sky-600 from-10% via-sky-900 via-90% to-sky-500 border dark:border-sky-900"
+            className="rounded-md px-5 py-2 cursor-pointer opacity-70 bg-gradient-to-t dark:from-blue-600 from-sky-400 from-10% dark:via-blue-900 via-sky-700 via-90% dark:to-blue-500 to-sky-400 border border-sky-400 dark:border-blue-900"
             onClick={() => {
-              setStartDateStorage(moment())
-              setStreak(0)
+              if(!startDateStorage){
+                setStartDateStorage(moment())
+                setStreak(0)
+
+              }
+              else{
+                setStartDateStorage(undefined)
+                  setRelapse(undefined)
+                  setStreak(0)
+                  setAtomDays(new Array(LastOfMonth.day).fill(true))
+              }
 
             }}
-          >{MainContent.start}</M.button>
-          <M.button initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            whileTap={{ scale: 0.9 }}
-            className={classNames("rounded-md  px-5 py-2 ",
-              { "bg-gradient-to-t from-shiraz-600 from-10% via-shiraz-900 via-90% to-shiraz-500 t-90% border dark:border-shiraz-900 cursor-pointer": startDateStorage },
-              { "bg-gradient-to-t from-woodSmoke-600 from-10% via-woodSmoke-900 via-90% to-woodSmoke-500 border dark:border-woodSmoke-900": !startDateStorage }
-
-            )}
-            disabled={!startDateStorage}
-            onClick={() => {
-              if (startDateStorage) {
-                setAtomDays((prev) => {
-                  const newArray = prev.slice()
-                  const today = new DateObject({ date: new Date, locale: persian_fa, calendar: persian })
-                  newArray[today.day - 1] = !newArray[today.day - 1]
-                  return newArray
-                })
-                setRelapse(useTracker.currentDate)
-              }
-            }
-            }
-          >
-            {MainContent.relapse}
-          </M.button>
+          >{startDateStorage ? MainContent.clear:MainContent.start }</M.button>
+         
 
         </M.div>
         {startDateStorage 
